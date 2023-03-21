6ae2c143aeacc6e584341278e196aa7c1388bcf3
DylanButton <dylanbutton1@gmail.com>
2023-03-21 18:01:31 +0000

    random number guessing game

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Random rnd = new Random();
+int num = rnd.Next(10);
+Console.WriteLine(num);
+Console.WriteLine("Guess the number:");
+int guessNum = Convert.ToInt32(Console.ReadLine());
+if (guessNum == num)
+{
+    Console.WriteLine("Correct! the number was " + num);
+}
+else
+{
+    Console.WriteLine("wrong ");
+
+}
